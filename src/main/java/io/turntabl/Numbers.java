86c7cf22f25c21d64892ed27426d52834eab6ccb
AfiMaameDufie <afimaamedufie@gmail.com>
2020-10-08 16:56:19 +0000

    Afi added a comment about the numbers class

--- src/main/java/io/turntabl/Numbers.java
+++ src/main/java/io/turntabl/Numbers.java
@@ -1,10 +1,12 @@
 package io.turntabl;
 
+//So basically, this is a class called Numbers and it returns a boolean value based on the arguments passed to it.
+
 public class Numbers {
     public static boolean isGreaterThanZeroAndLessThanAThousand(int i){
         return greaterThanZero(i) && lessThanAThousand(i);
     }
-
+           
     private static boolean greaterThanZero(int i){
         return i > 0;
     }
